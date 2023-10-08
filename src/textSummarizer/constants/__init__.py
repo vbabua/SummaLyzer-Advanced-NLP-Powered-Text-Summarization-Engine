184dd16e7b6a2565b820509fd6ef89dd5ab1f9e6
yoonpaix <babuvadakemu@gmail.com>
2023-10-08 11:51:13 +0530

    Adding file containing constants

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,8 @@
+
+from pathlib import Path
+
+# Path to the main configuration file for the project.
+CONFIG_FILE_PATH = Path("config/config.yaml")
+
+# Path to the parameters file that might contain hyperparameters, settings, etc.
+PARAMS_FILE_PATH = Path("params.yaml")
